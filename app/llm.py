6daf96e403b6288189a3f3241ff1ae44da17b036
Nitesh Kumar Varma <varnityakumar5722@gmail.com>
2026-01-04 21:35:28 +0530

    Update llm.py

--- app/llm.py
+++ app/llm.py
@@ -1,4 +1,5 @@
-from google import genai
+import google.generativeai as genai
+
 from langchain_core.messages import AIMessage, HumanMessage
 from langsmith import traceable
 from typing import List
